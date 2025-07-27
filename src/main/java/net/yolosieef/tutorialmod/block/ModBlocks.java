a9681349fd54cd5cd129a9945acfe2d2e3b9f9c3
WidnerJosef <josef.widner@gmail.com>
2025-07-27 17:45:12 +0200

    added item dropping and crafting recipes

--- src/main/java/net/yolosieef/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/yolosieef/tutorialmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.yolosieef.tutorialmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.yolosieef.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -23,6 +25,19 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create()
                     .strength(3.0f)
                     .requiresTool()));
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create()
+                            .strength(3.0f)
+                            .requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create()
+                            .strength(4.0f)
+                            .requiresTool()
+                            .sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
